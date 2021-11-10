1b8af203da72996c5795aba57fa963a58efd743a
usaruner <henryfeng500@gmail.com>
2021-11-10 00:07:57 -0800

    push

--- Lidar_Connection/edge_client.py
+++ Lidar_Connection/edge_client.py
@@ -1,5 +1,25 @@
 import socket
 dir = '/home/odin/Test/Senior-Design/ws_livox'
 
-class EdgeClient(Thread):
-	def __init__(self, ip='localhost', port=1028, vehicle=None):
+class EdgeClient():
+    def __init__(self, ip='127.0.0.1', port=6000, vehicle=None):
+        self.ip = ip
+        self.port = port
+        self.FRAME_SIZE = 16
+        self.send_frame_size = True
+        server_address = (ip, port)
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect the socket to the port where the server is listening
+        server_address = (ip, port)
+        print(sys.stderr, 'connecting to %s port %s' % server_address)
+        sock.connect(server_address)
+        try:
+            print("connected")
+            data = b""
+            while(data.decode() != "exit"):
+                sock.sendall(input().encode())
+                data = sock.recv(1024)
+        finally:
+            sock.close()
+        print('Received', repr(data))
--- Lidar_Connection/edge_server.py
+++ Lidar_Connection/edge_server.py
@@ -3,23 +3,67 @@ import socket
 #Jetson Nano: 10.42.0.23
 #Alex Laptop: 192.168.1.58
 #defaultLocation = ("10.42.0.23", 80)
+import socket
+import os
+import sys
+import subprocess
+import commands
+
+HOST = '192.168.1.58'  # Standard loopback interface address (localhost)
+PORT = 1028        # Port to listen on (non-privileged ports are > 1023)
+
+
+#Jetson Nano: 10.42.0.23
+#Alex Laptop: 192.168.1.58
+#defaultLocation = ("10.42.0.23", 80)
 
 HOST = '192.168.1.58'  # Standard loopback interface address (localhost)
 PORT = 1028        # Port to listen on (non-privileged ports are > 1023)
 
 class edge_server():
-    def __init__(self, ip='localhost', port=1028):
+    def __init__(self, ip='127.0.0.1', port=6000):
         self.ip = ip
         self.port = port
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((ip,port))
-        sock.listen(1)
-
-        while True:
-            # Wait for a connection
-            print >> sys.stderr, 'waiting for a connection'
-            connection, client_address = sock.accept()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.settimeout(120)
+        self.sock.bind((ip,port))
+        self.sock.listen(1)
+        self.sub = commands.livox_python()
+        print(self.sub.dir)
+        stop = True
+        try:
+            while stop:
+                # Wait for a connection
+                print(stop)
+                print(sys.stderr, 'waiting for a connection')
+                connection, client_address = self.sock.accept()
+                print("connected")
+                while stop:
+                    data = connection.recv(1024)
+                    print(data.decode(),data.decode() == "exit")
+                    print(stop)
+                    if not data: 
+                        stop = False
+                        break
+                    connection.sendall(data)
+                    if data.decode() == "exit": 
+                        self.sub.terminate_all()
+                        stop = False
+                        break
+                    if data.decode() == "connect": 
+                        self.sub.connect_lidar()
+                        break
+                    if data.decode() == "start": 
+                        self.sub.start_record()
+                        break
+                connection.sendall("exit".encode())
+            print("close")
+        finally:
+            self.sock.close()
+            connection.close()
+    def close():
+        sock.close()
     
 
     
